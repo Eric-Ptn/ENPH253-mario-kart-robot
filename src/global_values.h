c5398ecb195589dd9cd09c3c2d74d0e1b3cb884e
Eric Petersen <eric.ptn.ca@gmail.com>
2023-07-11 22:04:50 -0700

    feat: restructure code, add analog PID control, add compatibility with numerous IR sensors

---
+++ src/global_values.h
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+// pins
+#define SERVO_PIN 9 
+
+// IR sensor pins
+const byte IR_PINS[] = {A0, A1, A2, A3};
+const int NUM_IR_SENSORS = sizeof(IR_PINS);
+
+// steering PID values
+// get this to map to potentiometer values later for tuning?
+const int Kp = 5;
+const int Ki = 0;
+const int Kd = 0;
---
+++ src/servo_steering.h
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+#include <Servo.h>
+#include <global_values.h>
+
+/*
+This is just an initial attempt to code the control system for tape following (I would try to make this better but sick)
+Potential improvements:
+- Add a history for the error (perhaps an array that has previous errors, if the error is increasing, steer harder)
+   - Eric's ridicule: i thought about this too, but i think this is effectively just increasing integral control
+
+- Maybe alter this to work with only 2 IR sensors (maybe this would make the code run faster, but honestly, I don't know if it will
+  matter much)
+   - Eric's ridicule: code should now work for any number of IR sensors
+*/
+
+// variables for PID control, "static" ensures variables only have local file scope
+static int proportional = 0;
+static int integral = 0;
+static int derivative = 0;
+static int last_error = 0;
+static int max_integral = 100; // wind-up safety
+static int ir_readings[NUM_IR_SENSORS];
+static double desired_center = NUM_IR_SENSORS / 2 + 0.5; // position indices in weighted average start from 1, so add 0.5 to get the center
+
+void steer(Servo servo) {
+  // analog readings
+  for(int i = 0; i < NUM_IR_SENSORS; i++){
+    ir_readings[i] = analogRead(IR_PINS[i]);
+  }
+
+  // do weighted average to find current center of robot, ASSUMES EQUAL SPACING BETWEEN SENSORS
+  double current_position = 0;
+  int sum_of_weights = 0;
+  for(int i = 1; i <= NUM_IR_SENSORS; i++){  // cannot be zero-indexed because it screws up the weighted average
+    current_position += ir_readings[i - 1] * i; // careful here! i is 1-indexed, ir_readings is 0-indexed
+    sum_of_weights += ir_readings[i - 1];
+  }
+
+  current_position /= sum_of_weights; // a decimal from 1 to NUM_IR_SENSORS representing the current position of the tape relative to robot
+  int error = current_position - desired_center;
+  
+  // compute PID components
+  proportional = error;
+  integral = max(integral + error, max_integral);
+  derivative = last_error - error;
+  last_error = error;
+
+  int correction_val = Kp * proportional + Ki * integral + Kd * derivative;
+
+  // assumes that the servo is mounted at 90 degrees 
+  servo.write(90 - correction_val);
+}
